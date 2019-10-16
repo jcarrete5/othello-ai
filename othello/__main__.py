1d7100906378586478ef8fc9b735a8d2157ca66b
Jason R. Carrete <jasoncarrete5@gmail.com>
2019-10-16 00:29:19 -0400

    Add a note

--- othello/__main__.py
+++ othello/__main__.py
@@ -23,3 +23,8 @@ try:
     asyncio.run(main(), debug=True)
 finally:
     logging.shutdown()
+
+###- Some Notes -###
+# 3 kinds of players: NetworkPlayer, AIPlayer, ControlledPlayer
+# Each player has their own overriden loop method that determines how their move will be set
+# Remove the ai.loop method as that will not be how player moves are set
