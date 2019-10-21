dbe512e4d8f527e5f485e5312d60a7bdd5297c4f
Jason R. Carrete <Jason_Carrete@comcast.com>
2019-10-21 14:10:37 -0400

    Draw pieces on the canvas

--- othello/ui.py
+++ othello/ui.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import tkinter as tk
 import logging
 from typing import TYPE_CHECKING
+from othello import bitboard as bb
 
 if TYPE_CHECKING:
     from othello.game import BoardState
@@ -14,17 +15,21 @@ class BoardView:
         self.board_state = board_state
         self.cell_size = 40
         self.border_width = 3
-        self.grid_line_width = 2
+        self.grid_line_width = 1
         self.canvas = tk.Canvas(
             master,
             width=self.cell_size * 8 + self.grid_line_width * 7 - 1,
             height=self.cell_size * 8 + self.grid_line_width * 7 - 1,
             borderwidth=f'{self.border_width}',
+            highlightthickness=0,
             relief=tk.RAISED,
             bg='#000'
         )
         self.canvas.pack()
         self.canvas.bind('<Button-1>', BoardView.on_click)
+        _LOGGER.debug('Canvas dim (%d, %d)',
+                      self.canvas.winfo_reqwidth(),
+                      self.canvas.winfo_reqheight())
         self.redraw(board_state.white, board_state.black)
 
     @staticmethod
@@ -33,17 +38,35 @@ class BoardView:
 
     def redraw(self, white: int, black: int):
         _LOGGER.debug('Redrawing Board with white=%0#16x, black=%0#16x', white, black)
+        self.canvas.delete(tk.ALL)
         for r in range(8):
             for c in range(8):
-                col_off = c * self.grid_line_width + self.border_width
-                row_off = r * self.grid_line_width + self.border_width
-                rect = self.canvas.create_rectangle(
-                    c * self.cell_size + col_off,
-                    r * self.cell_size + row_off,
-                    (c + 1) * self.cell_size + col_off,
-                    (r + 1) * self.cell_size + row_off,
+                x_off = c * self.cell_size + c * self.grid_line_width + self.border_width - 1
+                y_off = r * self.cell_size + r * self.grid_line_width + self.border_width - 1
+                # Draw green empty cell
+                self.canvas.create_rectangle(
+                    x_off,
+                    y_off,
+                    self.cell_size + x_off,
+                    self.cell_size + y_off,
                     fill='#060'
                 )
+
+                # Draw piece in cell
+                mask = bb.pos_mask(r, c)
+                inset = 5
+                fill_color = None
+                if white & mask != 0:
+                    fill_color = '#fff'
+                elif black & mask != 0:
+                    fill_color = '#000'
+                if fill_color:
+                    self.canvas.create_oval(
+                        x_off + inset,
+                        y_off + inset,
+                        x_off + self.cell_size - inset,
+                        y_off + self.cell_size - inset,
+                        fill=fill_color)
 
 
 def init_widgets(root: tk.Tk, board_state: BoardState):
