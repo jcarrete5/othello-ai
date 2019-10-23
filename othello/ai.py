e18a06e8baf7309be54cc4a7e815535736c355d0
Jason R. Carrete <Jason_Carrete@comcast.com>
2019-10-23 14:54:06 -0400

    Implement random strategy for ai

--- othello/ai.py
+++ othello/ai.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
+from random import choice as chooseFrom
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from othello import bitboard as bb
     from othello.game import BoardState
 
-def random_strategy(state: BoardState):
-    ...
+def random(state: BoardState) -> bb.Position:
+    """ Returns a random valid move or None if no moves are possible. """
+    try:
+        return chooseFrom(state.valid_moves())
+    except IndexError:
+        return None
