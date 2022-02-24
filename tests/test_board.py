4f1f826bd902b484afe6332dbe10119fca1ce753
Jason Carrete <jasoncarrete5@gmail.com>
2022-02-24 00:54:59 -0500

    Add another board test

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import pytest
 
 from othelloai import bitboard as bb
@@ -8,9 +6,15 @@ from othelloai.player import Color
 from othelloai.exception import IllegalMoveError
 
 
-def test_place_and_capture():
+def test_place_illegal_move():
     board = Board()
     pos = bb.Position(0, 0)
     with pytest.raises(IllegalMoveError):
         board.place(Color.white, pos)
-    # TODO more tests
+
+
+def test_place_and_capture():
+    board = Board(init_white=0x0000101810200000, init_black=0x0000000028000000)
+    pos = bb.Position(2, 4)
+    board.place(Color.black, pos)
+    assert board.white == 0x0000100010200000 and board.black == 0x0000081828000000
