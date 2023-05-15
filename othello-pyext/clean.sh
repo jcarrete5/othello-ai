50676fed7014665d66475c787cbfda770057f044
Derek McBlane <mcblanederek@gmail.com>
2023-05-15 13:58:05 -0400

    remove build scripts

--- othello-pyext/clean.sh
+++
@@ -1,3 +0,0 @@
-BUILD_DIRS="build/ Release/ Debug/ othello_cpp.egg-info/"
-rm -rf ${BUILD_DIRS} 2>/dev/null
-
--- othello-pyext/othello-cpp/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BUILD_TYPE=${1:-Release}
-ctest --test-dir ${BUILD_TYPE} --output-on-failure
-
