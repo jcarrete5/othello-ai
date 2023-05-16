2a1712ae89dcd9ac3e2010f1378da74e914abaf5
Derek McBlane <mcblanederek@gmail.com>
2023-05-16 16:59:29 -0400

    update tests for aimax

--- tests/test_aimax_cpp.py
+++ tests/test_aimax_cpp.py
@@ -2,13 +2,18 @@ import pytest
 
 from threading import Event
 
-from othelloai.board import Board, Color
+from othelloai.game import Game, Board, Color
 from othelloai.ai.minmax_cpp import MinmaxAIPlayerCPP
+from othelloai.ai.random import RandomAIPlayer
 
-def test_place_and_capture():
+def test_move_generation_and_place():
     board = Board()
     black_player = MinmaxAIPlayerCPP(Color.black, 15)
     board.turn_player_color = Color.black
     move = black_player.get_move(board.copy(), Event())
     board.place(board.turn_player_color, move)
+
+def test_game():
+    game = Game(MinmaxAIPlayerCPP(Color.black, 15), RandomAIPlayer(Color.black), Board())
+    game.loop()
 
--- tests/test_board_cpp.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from othello_cpp import GameBoard
-
-def test_place_and_capture():
-    board = GameBoard()
-    assert(True)
-    
